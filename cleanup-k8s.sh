cff0bb8fdb678924ffb9fe444d3246e4aa45d0b0
Jay Desai <52437445+jay-nginx@users.noreply.github.com>
2021-01-06 11:36:24 +1100

    Update cleanup-k8s.sh

--- cleanup-k8s.sh
+++ cleanup-k8s.sh
@@ -1,5 +1,5 @@
 ## USE 
 ## curl -s https://raw.githubusercontent.com/jay-nginx/basic-workshop-udf/main/cleanup-k8s.sh | bash 
-kubeadm reset
+kubeadm reset -y
 iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 rm -rf ~/.kube/
